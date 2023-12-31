50ee9f86321c9a0f32cc0e65896e1057dbda48c9
yunimm <yuu9916@gmail.com>
2023-12-31 13:22:54 +0800

    feat : project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    modules: ['@nuxt/ui', 'nuxt-server-utils'],
+    ui: {},
+    nuxtServerUtils: {
+        mongodbUri: process.env.MONGODB_URI,
+    },
+});
