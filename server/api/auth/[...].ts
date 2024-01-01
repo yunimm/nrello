eaa9bf082d69dec4171d44f326dc3d2623e27268
yunimm <yuu9916@gmail.com>
2024-01-01 22:14:42 +0800

    feat : backend model / schema setting

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,66 @@
+import { NuxtAuthHandler } from '#auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { User } from '~/server/models/User.model';
+
+async function getUser(id: string) {
+    const user = await User.findById(id);
+    return user?.toJSON();
+}
+
+export default NuxtAuthHandler({
+    secret: useRuntimeConfig().auth.secret,
+    providers: [
+        //@ts-ignore
+        CredentialsProvider.default({
+            name: 'credentials',
+            origin: useRuntimeConfig().auth.origin,
+
+            async authorize(credentials: { email: string; password: string }) {
+                //login logic would go
+                const user = await User.findOne({ email: credentials.email });
+
+                if (!user) {
+                    return null;
+                }
+
+                const isValid = await user.comparePassword(
+                    credentials.password,
+                );
+
+                if (!isValid) {
+                    return null;
+                }
+
+                return user.toJSON();
+            },
+        }),
+    ],
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token = {
+                    ...token,
+                    ...user,
+                };
+            }
+
+            return token;
+        },
+
+        async session({ session, token }) {
+            //@ts-expect-error
+            const refreshedUser = await getUser(token._id);
+
+            session.user = {
+                ...token,
+                ...session.user,
+                ...refreshedUser,
+            };
+
+            return session;
+        },
+    },
+});
---
+++ server/utils/hash.ts
@@ -0,0 +1,7 @@
+import bcrypt from 'bcryptjs';
+
+export async function generateHash(data: string) {
+    const salt = await bcrypt.genSalt(10);
+    const hashData = await bcrypt.hash(data, salt);
+    return hashData;
+}
